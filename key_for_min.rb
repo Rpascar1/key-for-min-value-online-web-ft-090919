d508d21c9c1683d45d05c3fdc2e10d2d059944dc
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-14 00:18:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,12 @@
 def key_for_min_value(hash)
-  hash.each do |key,value|
-    if value < value
-      key
-end 
+  small_value = 0
+  small_key = nil
+
+    hash.each do |key,value|
+    if small_value == 0 || value < small_value
+    small_value = value
+    small_key = key
+    end
+  end
+  small_key
+end
